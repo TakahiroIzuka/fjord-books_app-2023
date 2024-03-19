7bcf44a738af8d052b48b61739ab7b074ad359e4
TakahiroIzuka <takahiroi.311@gmail.com>
2024-03-19 23:07:53 +0900

    最小限のユーザー一覧画面を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  resources :books
   devise_for :users
+  resources :users, :only => [:index]
+  resources :books
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
