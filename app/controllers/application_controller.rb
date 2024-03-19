bb0d9a72a18fd583be1e28a836664fd403a4a3df
TakahiroIzuka <takahiroi.311@gmail.com>
2024-03-19 22:26:26 +0900

    ログアウト機能を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource)
+    books_path
+  end
+
+  def after_sign_out_path_for(resource)
+    user_session_path
+  end
 end
