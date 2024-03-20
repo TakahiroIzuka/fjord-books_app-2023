077a43aa1eb0fb72d81d4fa8d50ee2e9e6ee3a67
TakahiroIzuka <takahiroi.311@gmail.com>
2024-03-20 12:13:19 +0900

    Sign up時に住所、郵便番号、自己紹介文を登録可能にする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
     books_path
@@ -8,5 +9,11 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource)
     user_session_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:address, :post_code, :introduction])
   end
 end
