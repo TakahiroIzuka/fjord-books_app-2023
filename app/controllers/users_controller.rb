4180df763a805af3d48f043a29637ad2d42c98cf
TakahiroIzuka <takahiroi.311@gmail.com>
2024-03-20 12:27:44 +0900

    ユーザー一覧にページネーションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.order(:id)
+    @users = User.order(:id).page(params[:page]).per(3)
   end
 
   def show
