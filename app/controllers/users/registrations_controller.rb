52efeae9088dc053c4271e9b78ca954d535b9472
TakahiroIzuka <takahiroi.311@gmail.com>
2024-03-21 15:11:12 +0900

    rubocopのwarning対応

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -30,7 +30,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  def after_update_path_for(resource)
+  def after_update_path_for(_resource)
     user_path(current_user)
   end
 
