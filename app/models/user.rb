d3a4cf5f718eab58abe16ea34d59412c5686a6ba
TakahiroIzuka <takahiroi.311@gmail.com>
2024-03-24 23:57:45 +0900

    png, jpg, gif以外の拡張子をアップロードできないようにする

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
+
+  validate :avatar_image_content_type, if: -> { avatar.attached? }
+
+  private
+
+  def avatar_image_content_type
+    extension = ['image/png', 'image/jpg', 'image/gif']
+    errors.add(extension.join(', '), " 以外の拡張子が指定されています。") unless avatar.content_type.in?(extension)
+  end
 end
