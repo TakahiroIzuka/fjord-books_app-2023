063cc26ea052a943c822a3c545d9429890124483
TakahiroIzuka <takahiroi.311@gmail.com>
2024-03-29 12:56:14 +0900

    クラスインスタンス変数名を@book->@commentableへ変更

--- app/controllers/books/comments_controller.rb
+++ app/controllers/books/comments_controller.rb
@@ -1,31 +1,30 @@
 # frozen_string_literal: true
 
 class Books::CommentsController < CommentsController
-  before_action :set_book, only: %i[new create]
+  before_action :set_commentable, only: %i[new create destroy]
 
   def create
-    @comment = @book.comments.new(comment_params)
+    @comment = @commentable.comments.new(comment_params)
     if @comment.save
       redirect_to @comment.commentable, notice: 'コメントを投稿しました'
     else
-      redirect_to book_path(@book)
+      redirect_to book_path(@commentable)
     end
   end
 
   def destroy
-    @book = Book.find(params[:book_id])
-    @comment = @book.comments.find(params[:id])
+    @comment = @commentable.comments.find(params[:id])
     if @comment.user_id != current_user.id
-      redirect_to book_path(@book), alert: '他のユーザーのコメントは削除できません'
+      redirect_to book_path(@commentable), alert: '他のユーザーのコメントは削除できません'
     end
 
     @comment.destroy
-    redirect_to book_path(@book), notice: 'コメントを削除しました'
+    redirect_to book_path(@commentable), notice: 'コメントを削除しました'
   end
 
   private
 
-  def set_book
-    @book = Book.find(params[:book_id])
+  def set_commentable
+    @commentable = Book.find(params[:book_id])
   end
 end
