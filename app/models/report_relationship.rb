d369b42eb0864bddbf00dba73a0f405f7684407c
TakahiroIzuka <takahiroi.311@gmail.com>
2024-03-31 15:34:49 +0900

    自身の日報は言及できないようにvalidateを追加

--- app/models/report_relationship.rb
+++ app/models/report_relationship.rb
@@ -3,4 +3,11 @@ class ReportRelationship < ApplicationRecord
   belongs_to :mentioning_report, class_name: 'Report'
   validates :report_id, presence: true
   validates :mentioning_report_id, presence: true
+  validate :not_self_mention
+
+  private
+
+  def not_self_mention
+    errors.add(:could_not_mention_oneself) if report_id == mentioning_report_id
+  end
 end
