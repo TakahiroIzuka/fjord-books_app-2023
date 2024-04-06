840588af711b7294b73d0601cc92493af1d8e0e7
TakahiroIzuka <takahiroi.311@gmail.com>
2024-04-06 16:05:06 +0900

    migrationファイル内で外部キーの定義はt.referencesを使うようにする

--- db/migrate/20240331021515_create_report_relationships.rb
+++ db/migrate/20240331021515_create_report_relationships.rb
@@ -1,13 +1,11 @@
 class CreateReportRelationships < ActiveRecord::Migration[7.0]
   def change
     create_table :report_relationships do |t|
-      t.integer :report_id
-      t.integer :mentioning_report_id
+      t.references :report
+      t.references :mentioning_report
 
       t.timestamps
     end
-    add_index :report_relationships, :report_id
-    add_index :report_relationships, :mentioning_report_id
     add_index :report_relationships, %i[report_id mentioning_report_id], unique: true
   end
 end
