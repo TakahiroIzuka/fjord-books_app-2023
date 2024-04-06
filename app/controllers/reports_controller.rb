31e632ca358b802585036b73f4f18b58baabfe63
TakahiroIzuka <takahiroi.311@gmail.com>
2024-04-06 16:12:21 +0900

    report_controller内でbefore_actionでオブジェクトをsetしないようにする

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class ReportsController < ApplicationController
-  before_action :set_report, only: %i[edit update destroy]
-
   def index
     @reports = Report.includes(:user).order(id: :desc).page(params[:page])
   end
@@ -16,11 +14,13 @@ class ReportsController < ApplicationController
     @report = current_user.reports.new
   end
 
-  def edit; end
+  def edit
+    @report = current_user.reports.find(params[:id])
+  end
 
   def create
-
     @report = current_user.reports.create(create_params)
+
     if @report.mention_reports(mentioning_reports) && @report.save
       redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
     else
@@ -29,6 +29,8 @@ class ReportsController < ApplicationController
   end
 
   def update
+    @report = current_user.reports.find(params[:id])
+
     ActiveRecord::Base.transaction do
       @report.update(update_params)
 
@@ -40,16 +42,13 @@ class ReportsController < ApplicationController
   end
 
   def destroy
+    @report = current_user.reports.find(params[:id])
     @report.destroy
 
     redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
   end
 
   private
-
-  def set_report
-    @report = current_user.reports.find(params[:id])
-  end
 
   def create_params
     params.require(:report).permit(:title, :content)
