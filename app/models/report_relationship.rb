94b67c358194096e30e754d47ef31866bb91c6d5
TakahiroIzuka <takahiroi.311@gmail.com>
2024-04-06 17:02:41 +0900

    不要なモデルバリデーションを削除

--- app/models/report_relationship.rb
+++ app/models/report_relationship.rb
@@ -3,8 +3,7 @@
 class ReportRelationship < ApplicationRecord
   belongs_to :report
   belongs_to :mentioning_report, class_name: 'Report'
-  validates :report_id, presence: true
-  validates :mentioning_report_id, presence: true
+
   validate :not_self_mention
 
   private
