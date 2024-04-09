a8369e9e3d980a24b984ab31f5897c650b09ab60
TakahiroIzuka <takahiroi.311@gmail.com>
2024-04-09 13:41:15 +0900

    mentioning_report_idsをクラスメソッドに変更

--- app/models/report.rb
+++ app/models/report.rb
@@ -36,7 +36,7 @@ class Report < ApplicationRecord
     end
   end
 
-  def mentioning_report_ids(content)
+  def self.mentioning_report_ids(content)
     report_ids = content.scan(%r{(http://localhost:3000/reports)/([0-9]+)})
     report_ids.filter_map { |url_array| url_array[1].to_i if url_array[1].to_i != id }.uniq
   end
